2da36b64e14529c469543041b05e6dae404a231a
Eugene Siow <kyo116@gmail.com>
2016-05-10 16:35:45 +0100

    Initial Commit

---
+++ src/main/java/sparql2fed/test.java
@@ -0,0 +1,43 @@
+package sparql2fed;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.apache.calcite.jdbc.CalciteConnection;
+
+public class test {
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.apache.calcite.jdbc.Driver");
+			Properties info = new Properties();
+			info.setProperty("lex", "JAVA");
+			info.setProperty("model", "model.json");
+			Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
+			CalciteConnection calciteConnection =
+			    connection.unwrap(CalciteConnection.class);
+			
+			Statement statement = calciteConnection.createStatement();
+			ResultSet resultSet = statement.executeQuery(
+			    "SELECT AIRTEMPERATURE FROM _ALT._ALT UNION ALL SELECT AIRTEMPERATURE FROM _AGPC1._AGPC1");
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while (resultSet.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+		            if (i > 1) System.out.print(",\t");
+		            String columnValue = resultSet.getString(i);
+		            System.out.print(columnValue);
+		        }
+		        System.out.println("");
+		    }
+			resultSet.close();
+			statement.close();
+			connection.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
